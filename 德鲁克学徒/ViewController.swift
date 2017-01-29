869a1fc0d32cea2873f128930e675ed76047c449
Oliver Zhang <oli.zhang@gmail.com>
2017-01-29 11:45:41 +0800

    initial commit

--- 德鲁克学徒/ViewController.swift
+++ 德鲁克学徒/ViewController.swift
@@ -7,12 +7,21 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIWebViewDelegate, WKNavigationDelegate {
+    var webView = WKWebView()
+    
+    override func loadView() {
+        super.loadView()
+        self.view = self.webView
+        self.webView.navigationDelegate = self
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadFromLocal()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +29,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func loadFromLocal() {
+        if let templatePath = Bundle.main.path(forResource: "index", ofType: "html", inDirectory: "dist"){
+            print(templatePath)
+            let url = URL(fileURLWithPath: templatePath)
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+    }
 
 }
 
