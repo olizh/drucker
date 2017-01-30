5a45dc44ba9c09d3e2475a070d2c39768a6d0096
Oliver Zhang <oli.zhang@gmail.com>
2017-01-30 20:16:59 +0800

    multiple choice

--- 德鲁克学徒/ViewController.swift
+++ 德鲁克学徒/ViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 import WebKit
 
 class ViewController: UIViewController, UIWebViewDelegate, WKNavigationDelegate {
-    var webView = WKWebView()
+    private var webView = WKWebView()
+    private var startUrl = "https://olizh.github.io/?isInSWIFT"
     
     override func loadView() {
         super.loadView()
         self.view = self.webView
         self.webView.navigationDelegate = self
-        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +30,16 @@ class ViewController: UIViewController, UIWebViewDelegate, WKNavigationDelegate 
     }
 
     func loadFromLocal() {
-        if let templatePath = Bundle.main.path(forResource: "index", ofType: "html", inDirectory: "dist"){
-            print(templatePath)
-            let url = URL(fileURLWithPath: templatePath)
-            let request = URLRequest(url: url)
-            webView.load(request)
+        if let templatePath = Bundle.main.path(forResource: "index", ofType: "html"){
+            let base = URL(string: startUrl)
+            do {
+                let s = try NSString(contentsOfFile:templatePath, encoding:String.Encoding.utf8.rawValue)
+                self.webView.loadHTMLString(s as String, baseURL:base)
+            } catch {
+                //loadFromWeb()
+            }
         }
     }
-
+    
 }
 
